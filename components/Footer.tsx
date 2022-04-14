e2bec7a0e71a0a8b385d3f0ad30f4a122736dbac
saurabhan <saurabhan.001@gmail.com>
2022-04-14 18:18:17 +0530

    [intial build] added homepage and video modal

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='flex items-center justify-center space-x-7 font-bold p-10'>
+            <h1>CineView</h1>
+            <p>All Rights Reserved 2022</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
